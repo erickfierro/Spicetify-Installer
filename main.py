b22d46aeb9d484d031122d0086703e69842ed705
= <stevperdomo2017@gmail.com>
2023-12-09 14:18:42 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# Archivo: main.py
+# Autor: Erick Stiven Fierro
+# Fecha: 9/12/2023
+# Descripción: Herramienta para instalar Spicetify/Spotify
+
+# Importamos modulos necesarios
+import os
+import subprocess
+import curses
+
+# Importamos todas las variables
+from variables import *
+
+
+if __name__ == "__main__":
+    '''Método principal del programa.'''
